291b97a71fc98cc718b7f4104dc04b4629f1bbe5
pierre <pnavarin@gmail.com>
2021-08-27 17:21:00 -0400

    Finished valid moves and new positions

--- Knights_travails/knights_travails.rb
+++ Knights_travails/knights_travails.rb
@@ -3,6 +3,23 @@ require_relative "00_tree_node.rb"
 class KnightPathFinder
 
     def self.valid_moves(pos)
+        moves = []
+        moves.push([(pos[0] + 2), (pos[1] + 1)]) #2,1
+        moves.push([(pos[0] + 1), (pos[1] + 2)]) #1,2
+        moves.push([(pos[0] - 2), (pos[1] + 1)]) #-2,1
+        moves.push([(pos[0] - 1), (pos[1] + 2)]) #-1,2
+        moves.push([(pos[0] - 2), (pos[1] - 1)]) #-2,-1
+        moves.push([(pos[0] - 1), (pos[1] - 2)]) #-1,-2
+        moves.push([(pos[0] + 2), (pos[1] - 1)]) #2,-1
+        moves.push([(pos[0] + 1), (pos[1] - 2)]) #1,-2
+
+        rows = (0..7).to_a
+        cols = (0..7).to_a
+
+
+        moves.select do |move|
+            rows.include?(move[0]) && cols.include?(move[1]) 
+        end
         
     end
 
@@ -12,11 +29,15 @@ class KnightPathFinder
         @considered_positions = [pos]
     end
 
-    def build_move_tree(node)
+    def build_move_tree(pos)
+        pos_children = new_move_positions(pos).map {|pos| PolyTreeNode.new(pos)}
 
     end
 
     def new_move_positions(pos)
-        KnightPathFinder.valid_moves(pos)
+        valid_positions = KnightPathFinder.valid_moves(pos)
+        new_arr = valid_positions.reject {|pos| @considered_positions.include?(pos)}
+        @considered_positions += new_arr
+        new_arr
     end
 end 
