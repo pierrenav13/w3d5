d5748a7b1218fb79c4d8112a08291046b8c80014
pierre <pnavarin@gmail.com>
2021-08-27 14:04:33 -0400

    initialized poly tree node

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,15 @@
 class PolyTreeNode
+    attr_reader :children, :value
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(value)
+        @parent = PolyTreeNode.new(value)
+        @parent.children << self
+    end
+
 
 end
